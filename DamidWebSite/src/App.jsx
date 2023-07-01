bd94ef17961fa53be24dec1f295b6d8fae6bf15d
jaleel <jaleel7aoug@gmail.com>
2023-07-01 11:11:44 +0200

    INIT

---
+++ DamidWebSite/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='BackgroundAbsolute'>
+        
+        </div>
+      <div className='TitleContainer'>
+
+      <div className='ContainerLogo'>
+<div className='TitleLogo'>
+
+</div>
+</div>
+        <div className='TitleDesign'>
+
+        </div>
+      </div>
+      <div className='GamePlayContainer'>
+
+      </div>
+      <div className='AgentsContainer'>
+
+      <div className='CompétencesContainer'>
+
+</div>
+<div className='SkinContainer'>
+
+</div>
+<div className='CatchPhrase'>
+
+</div>
+
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
