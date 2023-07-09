99dd1e018f9441c590225a80926421c1b5a819bf
jaleel <jaleel7aoug@gmail.com>
2023-07-09 17:40:34 +0200

    SiteOmbre

--- DamidWebSite/src/App.jsx
+++ DamidWebSite/src/App.jsx
@@ -1,45 +1,129 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [frameCount, setFrameCount] = useState(0);
+  const [divShadow, setShadow] = useState({});
+  const [Soleil, setSoleil] = useState({});
+  const ShadowX =50;
+  const ShadowY = 50;
+  const MaxScroll =document.documentElement.scrollHeight- window.innerHeight;
+  let animationFrameId;
+    var TitleContainer = document.getElementById('title')
+    var CatchyContainer = document.getElementsByClassName('catchy')
+  var AgentsContainer = document.getElementsByClassName('agents')
+  var SkinAccessoire = document.getElementsByClassName('skin')
+  var CrossPlatform = document.getElementsByClassName('cross')
+  var Tag = document.getElementsByClassName('tag')
+  var Ranks = document.getElementsByClassName('ranks')
+
+  useEffect(() => {
+  const updateFrame = () => {
+    setFrameCount((prevFrameCount) => prevFrameCount + 1);
+    var PourcentageScrollX =(((window.scrollY*100)/MaxScroll)/100)*ShadowX
+    var PourcentageScrollY =(((window.scrollY*100)/MaxScroll)/100)*ShadowY
+    changeShadow(PourcentageScrollX,PourcentageScrollY)
+    chnageSoleil((((window.scrollY*100)/MaxScroll)/100))
+    
+    animationFrameId = requestAnimationFrame(updateFrame);
+  };
+  updateFrame();
+  
+  return () => {
+    cancelAnimationFrame(animationFrameId);
+  };
+}, []);
+
+const changeShadow = (X,Y) => {
+  
+  var t = X-ShadowX/2;
+  if(Y >= ShadowY/2){
+    var y = ShadowY-Y
+  }else{
+    var y = Y
+  }
+  //console.log(y)
+  setShadow({
+    boxShadow: `${t}px ${y+13}px ${7}px rgba(0,0,0,0.7)`,
+    // Add any other style properties you want to change
+  });
+};
+
+const chnageSoleil = (X) => {
+  if(X >= 0.5){
+    var t = 1-X
+  }else{
+    var t = X
+  }
+
+  
+  var y = 1-(t*2)
+
+  var j = y*0.6+0.2
+ 
+  setSoleil({
+    backgroundColor:`rgba(29, 36, 84, ${j})`,
+  });
+};
+    
+  
 
   return (
     <>
+    <div className='Main'>
       <div className='BackgroundAbsolute'>
         
         </div>
-      <div className='TitleContainer'>
-
+        <div className='SousBackground' style={Soleil}></div>
+      <div className='Container' id='title' style={divShadow}>
+      <div className='TitleSousContainer' >
       <div className='ContainerLogo'>
-<div className='TitleLogo'>
+      <div className='TitleLogo'>
 
-</div>
-</div>
-        <div className='TitleDesign'>
+        </div>
+        </div>
+        
 
         </div>
       </div>
-      <div className='GamePlayContainer'>
+      <div className='Container' id='catchy' style={divShadow}>
+      <div className='CatchySousContainer' >
+        <div className='TitleCatchy'>A new way to play 2D game</div>
+        <div className=''></div>
+      </div>
+      </div>
+      <div className='Container' id='agents' style={divShadow}>
+      <div className='AgentsSousContainer'>
+      <div className='MIDS'></div>
+      
+      </div>
+      </div>
+      <div className='Container' id='skin' style={divShadow}>
+      <div className='SkinAccessoireSous'>
+      </div>
+      </div>
+      <div className='Container' id='cross' style={divShadow}>
+      <div className='SousCrossPlatform' >
+
+    </div>  
+      </div>
+
+      <div className='Container' id='tag' style={divShadow}>
 
       </div>
-      <div className='AgentsContainer'>
+      <div className='Container' id='ranks' style={divShadow}></div>
 
-      <div className='CompétencesContainer'>
+      <div className='CatchPhrase'>
 
-</div>
-<div className='SkinContainer'>
-
-</div>
-<div className='CatchPhrase'>
-
-</div>
+      </div>
 
       </div>
       
       
     </>
   )
+
+  
 }
 
 export default App
